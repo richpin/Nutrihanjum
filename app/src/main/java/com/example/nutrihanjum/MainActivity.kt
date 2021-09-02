d003ec996d6c4146a37def6cd38ae942faf175da
richpin <richpinkr99@gmail.com>
2021-09-02 22:01:29 +0900

    Create Splash, Icon, and Binding

--- app/src/main/java/com/example/nutrihanjum/MainActivity.kt
+++ app/src/main/java/com/example/nutrihanjum/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.nutrihanjum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nutrihanjum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //setTheme(R.style.Theme_Nutrihanjum)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
