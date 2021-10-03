771c4648f2e5450b60cdec020f79a73bc90900e8
KimJS0328 <wjddkel328@gmail.com>
2021-10-03 18:32:59 +0900

    dialog login activity

--- app/src/main/java/com/example/nutrihanjum/LoginActivity.kt
+++ app/src/main/java/com/example/nutrihanjum/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
@@ -31,6 +32,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
         setLoginListener()
